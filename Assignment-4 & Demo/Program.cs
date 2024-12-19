b87a69c3532bea04d5bd56c8e942b703e24c17ad
Mohamed Ramdan <mohamedramdan10.2001@gmail.com>
2024-12-20 00:12:49 +0200

    Q4

--- Assignment-4 & Demo/Program.cs
+++ Assignment-4 & Demo/Program.cs
@@ -245,7 +245,19 @@
             //}
             #endregion
 
-            
+            #region Q4
+            //Console.Write("Enter a Number : ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a power : ");
+            //int power = int.Parse(Console.ReadLine());
+            //int result = 1;
+            //for (int i = 1; i <= power; i++)
+            //{
+            //    result *= num;
+            //}
+            //Console.WriteLine($"The result of {num}^{power} is: {result}");
+
+            #endregion
             #endregion
         }
     }
